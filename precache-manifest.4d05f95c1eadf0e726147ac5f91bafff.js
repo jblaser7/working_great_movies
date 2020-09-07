f58c867adc2e8259e54273fa0c43573b039f3652
Jorge Jiménez <jblaser7@gmail.com>
2020-09-07 11:39:49 +0200

    Updates

---
+++ precache-manifest.4d05f95c1eadf0e726147ac5f91bafff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29a66224eb4d1d8c672e975878886db",
+    "url": "/working_great_movies/index.html"
+  },
+  {
+    "revision": "59985266fe8b84a03d0a",
+    "url": "/working_great_movies/static/css/main.9030adfe.chunk.css"
+  },
+  {
+    "revision": "943fe64328c170c43056",
+    "url": "/working_great_movies/static/js/2.356db000.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/working_great_movies/static/js/2.356db000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59985266fe8b84a03d0a",
+    "url": "/working_great_movies/static/js/main.b03ca989.chunk.js"
+  },
+  {
+    "revision": "8c80ad740b66ebd0a310",
+    "url": "/working_great_movies/static/js/runtime-main.212e0acb.js"
+  }
+]);
